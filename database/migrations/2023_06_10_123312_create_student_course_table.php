bc9d0775a1d842dcd8f1aaf1acb3a818a7740f92
kinana-sa <kinana.sa99@gmail.com>
2023-06-10 15:52:43 +0300

    first  commit

---
+++ database/migrations/2023_06_10_123312_create_student_course_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_course', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('course_id');
+            $table->primary(['user_id', 'course_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_course');
+    }
+};
