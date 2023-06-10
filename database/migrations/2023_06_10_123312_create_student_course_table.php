2d3be290edeb18601dde85adf69a0dfa4ab0f191
kinana-sa <kinana.sa99@gmail.com>
2023-06-10 19:00:38 +0300

    first  commit

--- database/migrations/2023_06_10_123312_create_student_course_table.php
+++ database/migrations/2023_06_10_123312_create_student_course_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('course_id');
             $table->primary(['user_id', 'course_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->timestamps();
         });
     }
